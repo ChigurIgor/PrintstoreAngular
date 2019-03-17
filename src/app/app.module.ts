75eab2f9b208b9c3c70c8885dbb23087316b2e85
IGOR CHIGUR <chigur.ig@gmail.com>
2019-03-17 20:38:14 +0200

    Add NavBar Top

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationBarTopComponent } from './navigation-bar-top/navigation-bar-top.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarTopComponent
   ],
   imports: [
     BrowserModule
